a442cae859dd722458229a0c89819e4f1b7fa8e6
Andrey Smelter <smelandr@gmail.com>
2017-04-14 13:07:23 -0400

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import re
+from setuptools import setup, find_packages, Extension
+
+if sys.argv[-1] == 'publish':
+    os.system('python3 setup.py sdist upload')
+    sys.exit()
+
+
+def readme():
+    with open('README.rst') as readme_file:
+        return readme_file.read()
+
+
+def find_version():
+    with open('jpredapi/__init__.py', 'r') as fd:
+        version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                            fd.read(), re.MULTILINE).group(1)
+    if not version:
+        raise RuntimeError('Cannot find version information')
+    return version
+
+
+REQUIRES = [
+    "docopt >= 0.6.2",
+    "requests >= 2.13.0",
+    "retrying >= 1.3.3"
+]
+
+
+setup(
+    name='jpredapi',
+    version=find_version(),
+    author='Andrey Smelter',
+    author_email='andrey.smelter@gmail.com',
+    description='Python library for submitting jobs to JPRED - A Protein Secondary Structure Prediction Server',
+    keywords='JPRED JPRED REST API',
+    license='MIT',
+    url='https://github.com/MoseleyBioinformaticsLab/jpredapi',
+    packages=find_packages(),
+    platforms='any',
+    long_description=readme(),
+    install_requires=REQUIRES,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
