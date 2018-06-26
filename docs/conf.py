d36459c957d7cfd2fd24c4325e55451db77cf585
Andrey Smelter <andrey.smelter@gmail.com>
2018-06-26 14:51:02 -0400

    Added intersphinx mapping to requests library.

--- docs/conf.py
+++ docs/conf.py
@@ -278,4 +278,5 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None),
+                       'requests': ('http://docs.python-requests.org/en/master/', None)}
