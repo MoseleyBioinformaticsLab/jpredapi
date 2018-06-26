d6ed9422dcb043639f55f3ada463804c1d7a8e87
Andrey Smelter <andrey.smelter@gmail.com>
2018-06-26 15:08:46 -0400

    Added missing arguments.

--- jpredapi/cli.py
+++ jpredapi/cli.py
@@ -8,7 +8,7 @@ The RESTful API allows JPred users to submit jobs from the command-line.
 
 Usage:
     jpredapi -h | --help
-    jpredapi --version
+    jpredapi -v | --version
     jpredapi submit (--mode=<mode> --format=<format>) (--file=<filename> | --seq=<sequence>) [--email=<name@domain.com>] [--name=<job_name>] [--skipPDB=<value>] [--rest=<address>] [--jpred4=<address>] [--silent]
     jpredapi status (--job_id=<id>) [--results_dir=<path>] [--wait_interval=<interval>] [--extract] [--silent]
     jpredapi get_results (--job_id=<id>) [--results_dir=<path>] [--wait_interval=<interval>] [--extract] [--silent]
@@ -16,7 +16,7 @@ Usage:
 
 Options:
     -h, --help                   Show this help message.
-    --version                    Show jpredapi version.
+    -v, --version                Show jpredapi version.
     --silent                     Do not print messages.
     --extract                    Extract results tar.gz archive into folder.
     --mode=<mode>                Submission mode, possible values: single, batch, msa.
@@ -28,8 +28,8 @@ Options:
     --job_id=<job_id>            Job id.
     --skipPDB=<value>            PDB check, possible values: True, False [default: True].
     --results_dir=<path>         Path where to save archive with results.
-    --rest=<address>             REST address of server [default: http://www.compbio.dundee.ac.uk/jpred4/cgi-bin/rest].
-    --jpred4=<address>           Address of Jpred4 server [default: http://www.compbio.dundee.ac.uk/jpred4].
+    --rest=<address>             REST address of Jpred server [default: http://www.compbio.dundee.ac.uk/jpred4/cgi-bin/rest].
+    --jpred4=<address>           Address of Jpred server to retrieve results [default: http://www.compbio.dundee.ac.uk/jpred4].
     --wait_interval=<interval>   Wait interval before retrying to check job status in seconds [default: 60].
 """
 
@@ -49,19 +49,25 @@ def cli(cmdargs):
                    skipPDB=cmdargs["--skipPDB"],
                    email=cmdargs["--email"],
                    name=cmdargs["--name"],
-                   silent=cmdargs["--silent"])
+                   silent=cmdargs["--silent"],
+                   host=cmdargs["--rest"])
 
     elif cmdargs["status"]:
         api.status(job_id=cmdargs["--job_id"],
                    results_dir_path=cmdargs["--results_dir"],
                    extract=cmdargs["--extract"],
-                   silent=cmdargs["--silent"])
+                   silent=cmdargs["--silent"],
+                   host=cmdargs["--rest"],
+                   jpred4=cmdargs["--jpred4"])
 
     elif cmdargs["get_results"]:
         api.get_results(job_id=cmdargs["--job_id"],
                         results_dir_path=cmdargs["--results_dir"],
                         extract=cmdargs["--extract"],
-                        silent=cmdargs["--silent"])
+                        silent=cmdargs["--silent"],
+                        host=cmdargs["--rest"],
+                        jpred4=cmdargs["--jpred4"])
 
     elif cmdargs["quota"]:
-        api.quota(email=cmdargs["--email"])
+        api.quota(email=cmdargs["--email"],
+                  host=cmdargs["--host"])
