d0689bf8f77faab32a0351c645a0c3bf96485edc
Andrey Smelter <andrey.smelter@gmail.com>
2018-06-28 23:27:36 -0400

    Updated comman-line interface.

--- jpredapi/cli.py
+++ jpredapi/cli.py
@@ -7,30 +7,31 @@ jpredapi command-line interface
 The RESTful API allows JPred users to submit jobs from the command-line.
 
 Usage:
+    jpredapi submit (--mode=<mode> --format=<format>) (--file=<filename> | --seq=<sequence>) [--email=<name@domain.com>] [--name=<name>] [--rest=<address>] [--skipPDB] [--silent]
+    jpredapi status (--jobid=<id>) [--results=<path>] [--wait=<interval>] [--attempts=<max>] [--rest=<address>] [--jpred4=<address>] [--extract] [--silent]
+    jpredapi get_results (--jobid=<id>) [--results=<path>] [--wait=<interval>] [--attempts=<max>] [--rest=<address>] [--jpred4=<address>] [--extract] [--silent]
+    jpredapi quota (--email=<name@domain.com>)
     jpredapi -h | --help
     jpredapi -v | --version
-    jpredapi submit (--mode=<mode> --format=<format>) (--file=<filename> | --seq=<sequence>) [--email=<name@domain.com>] [--name=<job_name>] [--skipPDB=<value>] [--rest=<address>] [--jpred4=<address>] [--silent]
-    jpredapi status (--job_id=<id>) [--results_dir=<path>] [--wait_interval=<interval>] [--extract] [--silent]
-    jpredapi get_results (--job_id=<id>) [--results_dir=<path>] [--wait_interval=<interval>] [--extract] [--silent]
-    jpredapi quota (--email=<name@domain.com>)
 
 Options:
-    -h, --help                   Show this help message.
-    -v, --version                Show jpredapi version.
-    --silent                     Do not print messages.
-    --extract                    Extract results tar.gz archive into folder.
-    --mode=<mode>                Submission mode, possible values: single, batch, msa.
-    --format=<format>            Submission format, possible values: raw, fasta, msf, blc.
-    --file=<filename>            Filename of a file with the job input (sequence(s)).
-    --seq=<sequence>             Instead of passing input file, for single-sequence submission.
-    --email=<name@domain.com>    E-mail address where job report will be sent (optional for all but batch submissions).
-    --name=<job_name>            Job name.
-    --job_id=<job_id>            Job id.
-    --skipPDB=<value>            PDB check, possible values: True, False [default: True].
-    --results_dir=<path>         Path where to save archive with results.
-    --rest=<address>             REST address of Jpred server [default: http://www.compbio.dundee.ac.uk/jpred4/cgi-bin/rest].
-    --jpred4=<address>           Address of Jpred server to retrieve results [default: http://www.compbio.dundee.ac.uk/jpred4].
-    --wait_interval=<interval>   Wait interval before retrying to check job status in seconds [default: 60].
+    -h, --help                 Show this help message.
+    -v, --version              Show jpredapi version.
+    --silent                   Do not print messages.
+    --extract                  Extract results tar.gz archive.
+    --skipPDB                  PDB check.
+    --mode=<mode>              Submission mode, possible values: single, batch, msa.
+    --format=<format>          Submission format, possible values: raw, fasta, msf, blc.
+    --file=<filename>          Filename of a file with the job input (sequence(s)).
+    --seq=<sequence>           Instead of passing input file, for single-sequence submission.
+    --email=<name@domain.com>  E-mail address where job report will be sent (optional for all but batch submissions).
+    --name=<name>              Job name.
+    --jobid=<id>               Job id.
+    --results=<path>           Path to directory where to save archive with results.
+    --rest=<address>           REST address of server [default: http://www.compbio.dundee.ac.uk/jpred4/cgi-bin/rest].
+    --jpred4=<address>         Address of Jpred4 server [default: http://www.compbio.dundee.ac.uk/jpred4].
+    --wait=<interval>          Wait interval before retrying to check job status in seconds [default: 60].
+    --attempts=<max>           Maximum number of attempts to check job status [default: 10].
 """
 
 from . import api
