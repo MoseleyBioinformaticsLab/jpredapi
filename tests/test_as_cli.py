96bc60c02f750e3ec533de180d5c2d6bf5ec970a
Andrey Smelter <andrey.smelter@gmail.com>
2018-07-10 23:25:18 -0400

    Additional CLI test.

--- tests/test_as_cli.py
+++ tests/test_as_cli.py
@@ -19,7 +19,7 @@ SKIP_REAL_JPREDAPI = bool(int(os.getenv("SKIP_REAL_JPREDAPI", True)))
     "quota --email=name@domain.com --silent",
     "submit --mode=single --format=raw --seq=MQVWPIEGIKKFETLSYLPP",
     "submit --mode=single --format=raw --file=tests/example_data/single_raw.example",
-    "submit --mode=single --format=fasta --file=tests/example_data/single_raw.example",
+    "submit --mode=single --format=fasta --file=tests/example_data/single_fasta.example",
     "submit --mode=batch --format=fasta --file=tests/example_data/batch_fasta.example --email=name@domain.com",
     "submit --mode=msa --format=fasta --file=tests/example_data/msa_fasta.example --email=name@domain.com",
     "submit --mode=msa --format=msf --file=tests/example_data/msa_msf.example --email=name@domain.com",
@@ -27,6 +27,7 @@ SKIP_REAL_JPREDAPI = bool(int(os.getenv("SKIP_REAL_JPREDAPI", True)))
     "status --jobid=jp_K46D05A",
     "status --jobid=jp_K46D05A --results=jpred_sspred/results",
     "status --jobid=jp_K46D05A --results=jpred_sspred/results --extract",
+    "get_results --jobid=jp_K46D05A",
     "get_results --jobid=jp_K46D05A --results=jpred_sspred/results",
     "get_results --jobid=jp_K46D05A --results=jpred_sspred/results --extract"
 ])
